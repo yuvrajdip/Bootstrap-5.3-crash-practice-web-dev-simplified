8f5a155a16717ac90ad849ffcbc2646754090c9c
yuvrajdip <dipbhowmik2018@gmail.com>
2023-04-22 23:55:25 +0600

    first commit

---
+++ notes.js
@@ -0,0 +1,12 @@
+
+//* Columns : 
+//* only col-auto _->this is the only thing 
+//* which is gonna change the size of the 
+//*column box based on the content inside it
+
+
+//* If you don't hard code a size for what 
+//* it should do on smaller screen sizes it's 
+//* just going to take up the full width no  
+//* matter what 
+//* example : col-xl-2 col-lg-4 
